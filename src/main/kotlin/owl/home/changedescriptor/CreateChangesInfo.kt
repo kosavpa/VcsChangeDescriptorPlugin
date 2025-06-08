c0c720ec56b80d2a8b04a0b8e1cd9d1c362bebfa
kosavpa <kosavpa@yandex.ru>
2025-06-08 22:37:59 +0400

    init

---
+++ src/main/kotlin/owl/home/changedescriptor/CreateChangesInfo.kt
@@ -0,0 +1,85 @@
+package owl.home.changedescriptor
+
+
+import com.intellij.collaboration.ui.util.name
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.vcs.CheckinProjectPanel
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.ui.Refreshable
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.dsl.builder.Cell
+import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.builder.selected
+
+
+class CreateChangesInfo : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+
+        val vcsPanel = e.dataContext.getData(Refreshable.PANEL_KEY) as? CheckinProjectPanel ?: return
+
+        val changeList = ChangeListManager.getInstance(project)
+            .getChangeList(project.projectFile ?: return)
+            ?: return
+
+        val fileNameToMap = changeList
+            .changes
+            .filter { it.virtualFile?.name != null }
+            .associateBy { it.virtualFile!!.name }
+
+        val dialog = ApproveCommitMessageTextDialog(fileNameToMap.keys, project)
+
+        if (dialog.showAndGet()) {
+            vcsPanel.commitMessage = dialog.getApprovedFileNames()
+                .asSequence()
+                .map { fileNameToMap[it] }
+                .groupBy { it?.type?.name }
+                .map { entry ->
+                    "[%s]:\n%s".format(
+                        entry.key,
+                        entry.value
+                            .map { it?.virtualFile?.name }
+                            .fold("\t") { acc, s -> acc.plus(s).plus("\n\t") }
+                    )
+                }
+                .reduce { acc, s -> acc.plus("\n").plus(s) }
+        }
+    }
+
+
+    internal class ApproveCommitMessageTextDialog(private val commitFileNames: Set<String>, project: Project) :
+        DialogWrapper(project) {
+        private var checkBoxes: MutableList<Cell<JBCheckBox>> = mutableListOf()
+
+        init {
+            isResizable = true
+
+            init()
+
+            okAction.name = "Ок"
+            cancelAction.name = "Отмена"
+
+        }
+
+        fun getApprovedFileNames(): List<String> {
+            return checkBoxes.filter { it.component.isSelected }.map { it.component.text }
+        }
+
+        override fun createCenterPanel(): DialogPanel = panel {
+            commitFileNames.forEach {
+                row {
+                    val checkBox = checkBox(it)
+
+                    checkBox.selected(true)
+
+                    checkBoxes.add(checkBox)
+                }.resizableRow()
+            }
+        }
+
+    }
+}
